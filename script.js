2f5ef230d33706981f908a42d185dccedfc10fea
Hinduja-27 <hindujakorpuri@gmail.com>
2024-10-08 14:11:54 +0530

    add a new file

---
+++ script.js
@@ -0,0 +1,20 @@
+// compound inyrest calculate
+
+function calculate(){
+    const totalAmount = document.getElementById("total-amount");
+    const principalInput = document.getElementById("principal");
+    const rateInput= document.getElementById("Intrest Rate");
+    const yearsInput= document.getElementById("Years");
+
+
+    let principal = Number(principalInput.value);
+    let rate = Number(rateInput.value);
+    let years = Number(yearsInput.value*12);
+    
+    let  Intrest  = ((principal*rate*years)/100);
+
+    const result = principal+Intrest;
+
+    totalAmount.textContent = result.toLocaleString(undefined,{style:"currency",currency:"INR"});
+
+}
